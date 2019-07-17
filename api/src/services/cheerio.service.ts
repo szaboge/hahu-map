246ac9b606f3ac47373bdac64bc66de0d973b37e
Gergely Szabo <szagege@gmail.com>
2019-07-17 15:46:13 +0200

    feat(backend): Remove backend

--- api/src/services/cheerio.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-const cheerio = require('cheerio');
-
-@Injectable()
-export class CheerioService {
-    public parse(body: string) {
-        return cheerio.load(body);
-    }
-}
